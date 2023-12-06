2ef9604b21fa22e34853c3de3a51ca31543d5b07
Ocho93 <ar.sanchez.p93@gmail.com>
2023-12-06 01:17:35 +0100

    app libros

--- JS/script.js
+++ JS/script.js
@@ -21,24 +21,31 @@ function printData(books) {
   books.forEach(book => {
     // Crear un contenedor para cada tarjeta de libro
     const card = document.createElement('div');
-    card.classList.add('col');
+    card.classList.add('card', 'mb-3', 'mx-auto'); // Añadir clases Bootstrap para estilizar la tarjeta
 
     // Construir el contenido HTML para cada tarjeta de libro
     const cardContent = `
-      <div class="card">
-        <img src="${book.volumeInfo.imageLinks.thumbnail}" class="card-img" alt="Book Cover">
-        <div class="card-body">
-          <h5 class="card-title">${book.volumeInfo.title}</h5>
-          <p class="card-text">${book.volumeInfo.description}</p>
-          <p class="card-text"><strong>Author(s):</strong> ${book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown'}</p>
-        </div>
+      <div class="row g-0">
+      <div class="col-md-4">
+      <img src="${book.volumeInfo.imageLinks.thumbnail}" class="img-fluid rounded-start card-img" alt="Book Cover">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${book.volumeInfo.title}</h5>
+        <p class="card-text">${book.volumeInfo.description ? book.volumeInfo.description.substring(0, 150) + '...' : 'Description not available'}</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated ${book.volumeInfo.publishedDate ? book.volumeInfo.publishedDate : 'Unknown'}</small></p>
       </div>
-    `;
+    </div>
+  </div>
+`;
 
-    // Establecer el contenido HTML en el contenedor de la tarjeta de libro
-    card.innerHTML = cardContent;
-
-    // Agregar la tarjeta de libro al elemento principal en el DOM
+card.innerHTML = cardContent;
     main.appendChild(card);
   });
 }
+      
+
+
+
+
+
