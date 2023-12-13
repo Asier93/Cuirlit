31a1f0ad2a67dbe588afd913d3072e75c3d18491
Ocho93 <ar.sanchez.p93@gmail.com>
2023-12-13 23:43:45 +0100

    navbar

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,51 @@
 // Término de búsqueda para feminismo transinclusivo y queer
-const searchInput = 'feminismo transinclusivo queer';
+const searchInput = "feminismo transinclusivo queer";
 
 // Construir la URL de la API con el término de búsqueda
-const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(searchInput)}`;
+const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+  searchInput
+)}`;
 
 // Obtener el elemento principal en el DOM
 const main = document.getElementById("main");
 
 // Hacer la solicitud a la API de Google Books
 fetch(url)
-  .then(response => response.json()) // Convertir la respuesta a formato JSON
-  .then(response => printData(response.items)) // Llamar a la función printData con los elementos de libros
-  .catch(error => {
-    console.log('Error al hacer la solicitud:', error);
+  .then((response) => response.json()) // Convertir la respuesta a formato JSON
+  .then((response) => printData(response.items)) // Llamar a la función printData con los elementos de libros
+  .catch((error) => {
+    console.log("Error al hacer la solicitud:", error);
   });
 
 // Función para mostrar los datos de los libros en la página
 function printData(books) {
   // Iterar sobre cada libro recibido en la respuesta
-  books.forEach(book => {
+  books.forEach((book) => {
     // Crear un contenedor para cada tarjeta de libro
-    const card = document.createElement('div');
-    card.classList.add('card', 'mb-3', 'mx-auto'); // Añadir clases Bootstrap para estilizar la tarjeta
+    const card = document.createElement("div");
+    card.classList.add("card", "mb-3", "mx-auto"); // Añadir clases Bootstrap para estilizar la tarjeta
 
     // Construir el contenido HTML para cada tarjeta de libro
     const cardContent = `
       <div class="row g-0">
       <div class="col-md-4">
-      <img src="${book.volumeInfo.imageLinks.thumbnail}" class="img-fluid rounded-start card-img" alt="Book Cover">
+      <img src="${
+        book.volumeInfo.imageLinks.thumbnail
+      }" class="img-fluid rounded-start card-img" alt="Book Cover">
     </div>
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">${book.volumeInfo.title}</h5>
-        <p class="card-text">${book.volumeInfo.description ? book.volumeInfo.description.substring(0, 150) + '...' : 'Description not available'}</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated ${book.volumeInfo.publishedDate ? book.volumeInfo.publishedDate : 'Unknown'}</small></p>
+        <p class="card-text">${
+          book.volumeInfo.description
+            ? book.volumeInfo.description.substring(0, 150) + "..."
+            : "Description not available"
+        }</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated ${
+          book.volumeInfo.publishedDate
+            ? book.volumeInfo.publishedDate
+            : "Unknown"
+        }</small></p>
       </div>
     </div>
   </div>
@@ -45,39 +57,39 @@ function printData(books) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const cards = document.querySelectorAll('.card'); // Obtén todas las tarjetas
+  const cards = document.querySelectorAll(".card"); // Obtén todas las tarjetas
   const cardsPerPage = 6; // Cantidad de tarjetas a mostrar por página
-  const cardContainer = document.getElementById('cardContainer');
-  const pagination = document.getElementById('pagination');
+  const cardContainer = document.getElementById("cardContainer");
+  const pagination = document.getElementById("pagination");
 
   function displayCards(startIndex, endIndex) {
-      cards.forEach((card, index) => {
-          if (index >= startIndex && index < endIndex) {
-              card.style.display = 'block';
-          } else {
-              card.style.display = 'none';
-          }
-      });
+    cards.forEach((card, index) => {
+      if (index >= startIndex && index < endIndex) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
   }
 
   function generatePagination() {
-      const pageCount = Math.ceil(cards.length / cardsPerPage);
+    const pageCount = Math.ceil(cards.length / cardsPerPage);
 
-      let paginationButtons = '';
-      for (let i = 1; i <= pageCount; i++) {
-          paginationButtons += `<button class="page-link" onclick="changePage(event, ${i})">${i}</button>`;
-      }
+    let paginationButtons = "";
+    for (let i = 1; i <= pageCount; i++) {
+      paginationButtons += `<button class="page-link" onclick="changePage(event, ${i})">${i}</button>`;
+    }
 
-      pagination.innerHTML = paginationButtons;
+    pagination.innerHTML = paginationButtons;
   }
 
   function changePage(event, pageNumber) {
-      event.preventDefault(); // Evitar el comportamiento predeterminado del enlace (botón)
-      
-      const startIndex = (pageNumber - 1) * cardsPerPage;
-      const endIndex = startIndex + cardsPerPage;
+    event.preventDefault(); // Evitar el comportamiento predeterminado del enlace (botón)
 
-      displayCards(startIndex, endIndex);
+    const startIndex = (pageNumber - 1) * cardsPerPage;
+    const endIndex = startIndex + cardsPerPage;
+
+    displayCards(startIndex, endIndex);
   }
 
   displayCards(0, cardsPerPage); // Mostrar las primeras tarjetas al cargar la página
